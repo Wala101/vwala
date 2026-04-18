70b1ed1ef5423a2486852e27701a5ade4a8d5120
Moises Pereira <moiseskyo6543210@gmail.com>
2026-04-18 00:37:28 -0300

    Adiciona log temporario para validar firebase env no deploy

--- src/firebase.js
+++ src/firebase.js
@@ -11,6 +11,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
+console.log('FIREBASE_API_KEY:', import.meta.env.VITE_FIREBASE_API_KEY)
+
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 const db = getFirestore(app)
